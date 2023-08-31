88636295a9930d3f809b8a2101bac5ffc011e329
Evan Schoening <evan.schoening@gmail.com>
2023-08-31 05:47:22 -0700

    pcap listener that gets raw ip from header

--- sniffer/src/main.rs
+++ sniffer/src/main.rs
@@ -1,10 +1,11 @@
 use pcap::{Device, Capture};
+use etherparse::{PacketHeaders, SlicedPacket};
 
 
 fn main() {
     println!("Hello, world!");
-    let device_list = Device::list().unwrap();
-    println!("{:#?}", device_list);
+    // let device_list = Device::list().unwrap();
+    // println!("{:#?}", device_list);
     let main_device = Device::lookup().unwrap().unwrap();
     println!("{:#?}", main_device);
 
@@ -14,6 +15,12 @@ fn main() {
                 .open()
                 .unwrap();
     while let Ok(packet) = cap.next_packet(){
-        println!("Received packet: {:?}", packet)
+        //println!("Received packet: {:?}", packet);
+        match SlicedPacket::from_ethernet(&packet) {
+            Err(value) => println!("Err {:?}", value),
+            Ok(value) => {
+                println!("Value of ip: {:?}", value.ip)
+            }
+        }
     }
 }
